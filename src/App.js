e6ee1ffaacd9359b15a6e567b611022f37103a1c
John Manack <jmnck28@gmail.com>
2021-03-26 16:59:29 -0400

    begins form to create a new user account; incomplete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
+import NewNonMinorAccount from "./components/NewNonMinorAccount";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App"><NewNonMinorAccount/></div>;
 }
 
 export default App;
