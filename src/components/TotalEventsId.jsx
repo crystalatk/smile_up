0bf94c6d6f3ef3d78008f4bed88774e6661eee5c
Crystal Atkinson <crystaltayloratkinson@gmail.com>
2021-03-31 17:28:06 -0400

    Finished Profiles with adding total Volunteer Hours by ID and Total Events by ID onto page.

--- src/components/TotalEventsId.jsx
+++ src/components/TotalEventsId.jsx
@@ -1,36 +1,25 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
+const EventsId = ({ id }) => {
+  const [eventId, setEventId] = useState("");
+  useEffect(() => {
+    const getVH = () => {
+      fetch(`http://127.0.0.1:3232/events/totalEventsId?volunteer_id=${id}`)
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          setEventId(data[0].count);
+        });
+    };
+    getVH();
+  }, []);
 
-const EventsId = ({userInfo}) => {
-    const [eventId, setEventId] = useState('')
-    useEffect(() => {
-        
-        const getVH = () => {
-            fetch(`http://127.0.0.1:3232/events/totalEventsId?volunteer_id=${userInfo.id}`)
-            .then(res => res.json())
-            .then(data => {
-                console.log(data);
-                setEventId(data[0].count)
-            })
-        }
-        getVH();
-        
-    },[])
+  return (
+    <>
+      <h1>Total Events </h1>
+      {!!eventId ? <h2>{eventId}</h2> : <h3>0</h3>}
+    </>
+  );
+};
 
-
-    return (
-        <>
-            <h1>Total Events </h1>
-                {!!eventId ? 
-                <h2>
-                    {eventId}
-                </h2>
-                :<h3>nothing is here</h3>
-                }
-           
-        </>
-    )
-
-}
-
-export default EventsId
+export default EventsId;
--- src/components/VolunteerHrsById.jsx
+++ src/components/VolunteerHrsById.jsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from "react";
 
-const VHID = ({ userInfo }) => {
+const VHID = ({ id }) => {
   const [vhid, setVH] = useState("");
   useEffect(() => {
-    if (userInfo.id) {
+    if (id) {
       const getVH = () => {
         fetch(
-          `http://127.0.0.1:3232/volunteers/volunteerHoursId?volunteer_id=${userInfo.id}`
+          `http://127.0.0.1:3232/volunteers/volunteerHoursId?volunteer_id=${id}`
         )
           .then((res) => res.json())
           .then((data) => {
@@ -21,7 +21,7 @@ const VHID = ({ userInfo }) => {
   return (
     <>
       <h1>Total Hours </h1>
-      {!!vhid ? <h2>{vhid}</h2> : <h3>nothing is here</h3>}
+      {!!vhid ? <h2>{vhid}</h2> : <h3>0</h3>}
     </>
   );
 };
