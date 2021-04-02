dcc314a40c2d85abc463eddcf41848a487fee191
Joshua Garner <jgarner1989@yahoo.com>
2021-04-02 09:57:34 -0400

    start on eventlist list

--- src/components/EventList.jsx
+++ src/components/EventList.jsx
@@ -1,5 +1,27 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { makeStyles } from '@material-ui/core/styles';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Collapse from '@material-ui/core/Collapse';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
+import StarBorder from '@material-ui/icons/StarBorder';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+  },
+  nested: {
+    paddingLeft: theme.spacing(4),
+  },
+}));
+
 
 const EventList = () => {
   const [eventList, setEventList] = useState();
@@ -24,11 +46,95 @@ const EventList = () => {
     fetchList();
   }, []);
 
+  const classes = useStyles();
+  const [open, setOpen] = useState(true);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
+
+
   return (
     <>
       <h1>This is the Event List Page</h1>
       {!!eventList ? (
-        <>
+        <List
+        component="nav"
+        aria-labelledby="nested-list-subheader"
+        subheader={
+          <ListSubheader component="div" id="nested-list-subheader">
+            EVENTS
+          </ListSubheader>
+        }
+        className={classes.root}
+      >
+        <ListItem button onClick={handleClick}>
+        <ListItemIcon>
+          <InboxIcon />
+        </ListItemIcon>
+        <ListItemText primary="event1" />
+        {open ? <ExpandLess /> : <ExpandMore />}
+      </ListItem>
+      <Collapse in={open} timeout="auto" unmountOnExit>
+        <List component="div" disablePadding>
+          <ListItem button className={classes.nested}>
+            <ListItemIcon>
+              <StarBorder />
+            </ListItemIcon>
+            <ListItemText primary="detail1" />
+          </ListItem>
+          <ListItem button className={classes.nested}>
+            <ListItemIcon>
+              <StarBorder />
+            </ListItemIcon>
+            <ListItemText primary="detail2" />
+          </ListItem>
+          <ListItem button className={classes.nested}>
+            <ListItemIcon>
+              <StarBorder />
+            </ListItemIcon>
+            <ListItemText primary="detail3" />
+          </ListItem>
+          <ListItem button className={classes.nested}>
+            <ListItemIcon>
+              <StarBorder />
+            </ListItemIcon>
+            <ListItemText primary="detail4" />
+          </ListItem>
+          <ListItem button className={classes.nested}>
+            <ListItemIcon>
+              <StarBorder />
+            </ListItemIcon>
+            <ListItemText primary="detail5" />
+          </ListItem>
+          <ListItem button className={classes.nested}>
+            <ListItemIcon>
+              <StarBorder />
+            </ListItemIcon>
+            <ListItemText primary="detail6" />
+          </ListItem>
+          <ListItem button className={classes.nested}>
+            <ListItemIcon>
+              <StarBorder />
+            </ListItemIcon>
+            <ListItemText primary="detail7" />
+          </ListItem>
+          <ListItem button className={classes.nested}>
+            <ListItemIcon>
+              <StarBorder />
+            </ListItemIcon>
+            <ListItemText primary="detail8" />
+          </ListItem>
+          <ListItem button className={classes.nested}>
+            <ListItemIcon>
+              <StarBorder />
+            </ListItemIcon>
+            <ListItemText primary="detail8" />
+          </ListItem>
+        </List>
+      </Collapse>
+        {/* <>
           <ul>
             {eventList.map((event) => {
               return (
@@ -45,8 +151,9 @@ const EventList = () => {
                 </li>
               );
             })}
-          </ul>
-        </>
+          </ul> */}
+          </List>
+        // </>
       ) : (
         <>
           <h1>No Upcoming Events.</h1>
