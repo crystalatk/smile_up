370e5e3820809939a1780f0457c85264a51ca7ab
Crystal Atkinson <crystaltayloratkinson@gmail.com>
2021-04-02 14:19:06 -0400

    Small error fixes

--- src/components/EventList.jsx
+++ src/components/EventList.jsx
@@ -1,29 +1,27 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { makeStyles } from '@material-ui/core/styles';
-import ListSubheader from '@material-ui/core/ListSubheader';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import Collapse from '@material-ui/core/Collapse';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import ExpandLess from '@material-ui/icons/ExpandLess';
-import ExpandMore from '@material-ui/icons/ExpandMore';
-import StarBorder from '@material-ui/icons/StarBorder';
-import Moment from 'react-moment';
-
+import { makeStyles } from "@material-ui/core/styles";
+import ListSubheader from "@material-ui/core/ListSubheader";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import Collapse from "@material-ui/core/Collapse";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import ExpandLess from "@material-ui/icons/ExpandLess";
+import ExpandMore from "@material-ui/icons/ExpandMore";
+import StarBorder from "@material-ui/icons/StarBorder";
+import Moment from "moment";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    width: '100%',
+    width: "100%",
     maxWidth: 360,
   },
   nested: {
     paddingLeft: theme.spacing(4),
   },
 }));
-
 
 const EventList = () => {
   const [eventList, setEventList] = useState();
@@ -55,74 +53,74 @@ const EventList = () => {
     setOpen(!open);
   };
 
-
-
   return (
     <>
       <h1>This is the Event List Page</h1>
       {!!eventList ? (
         <List
-        component="nav"
-        aria-labelledby="nested-list-subheader"
-        subheader={
-          <ListSubheader component="div" id="nested-list-subheader">
-            EVENTS
-          </ListSubheader>
-        }
-        className={classes.root}
-      >
-        {eventList.map((event) => {
-              return (
-                <div key={event.id}>
-        <ListItem button onClick={handleClick}>
-        <ListItemIcon>
-          <InboxIcon />
-        </ListItemIcon>
-        <Link to={`/event/${event.id}`}>
-        <ListItemText primary={event.title}/>
-        </Link>
-        {open ? <ExpandLess /> : <ExpandMore />}
-      </ListItem>
-      <Collapse in={open} timeout="auto" unmountOnExit>
-        <List component="div" disablePadding>
-          <ListItem button className={classes.nested}>
-            <ListItemIcon>
-              <StarBorder />
-            </ListItemIcon>
-            <ListItemText primary={event.date} />
-          </ListItem>
-          <ListItem button className={classes.nested}>
-            <ListItemIcon>
-              <StarBorder />
-            </ListItemIcon>
-            <ListItemText primary={event.start_time +'-'+ event.stop_time} />
-          </ListItem>
-          <ListItem button className={classes.nested}>
-            <ListItemIcon>
-              <StarBorder />
-            </ListItemIcon>
-            <ListItemText primary={event.location}/>
-          </ListItem>
-          <ListItem button className={classes.nested}>
-            <ListItemIcon>
-              <StarBorder />
-            </ListItemIcon>
-            <ListItemText primary={event.deadline} />
-          </ListItem>
-          <ListItem button className={classes.nested}>
-            <ListItemIcon>
-              <StarBorder />
-            </ListItemIcon>
-            <ListItemText primary={event.age_min} />
-          </ListItem>
+          component="nav"
+          aria-labelledby="nested-list-subheader"
+          subheader={
+            <ListSubheader component="div" id="nested-list-subheader">
+              EVENTS
+            </ListSubheader>
+          }
+          className={classes.root}
+        >
+          {eventList.map((event) => {
+            return (
+              <div key={event.id}>
+                <ListItem button onClick={handleClick}>
+                  <ListItemIcon>
+                    <InboxIcon />
+                  </ListItemIcon>
+                  <Link to={`/event/${event.id}`}>
+                    <ListItemText primary={event.title} />
+                  </Link>
+                  {open ? <ExpandLess /> : <ExpandMore />}
+                </ListItem>
+                <Collapse in={open} timeout="auto" unmountOnExit>
+                  <List component="div" disablePadding>
+                    <ListItem button className={classes.nested}>
+                      <ListItemIcon>
+                        <StarBorder />
+                      </ListItemIcon>
+                      <ListItemText primary={event.date} />
+                    </ListItem>
+                    <ListItem button className={classes.nested}>
+                      <ListItemIcon>
+                        <StarBorder />
+                      </ListItemIcon>
+                      <ListItemText
+                        primary={event.start_time + "-" + event.stop_time}
+                      />
+                    </ListItem>
+                    <ListItem button className={classes.nested}>
+                      <ListItemIcon>
+                        <StarBorder />
+                      </ListItemIcon>
+                      <ListItemText primary={event.location} />
+                    </ListItem>
+                    <ListItem button className={classes.nested}>
+                      <ListItemIcon>
+                        <StarBorder />
+                      </ListItemIcon>
+                      <ListItemText primary={event.deadline} />
+                    </ListItem>
+                    <ListItem button className={classes.nested}>
+                      <ListItemIcon>
+                        <StarBorder />
+                      </ListItemIcon>
+                      <ListItemText primary={event.age_min} />
+                    </ListItem>
+                  </List>
+                </Collapse>
+              </div>
+            );
+          })}
         </List>
-      </Collapse>
-      </div>
-      );
-    })}
-          </List>
-        // </>
       ) : (
+        // </>
         <>
           <h1>No Upcoming Events.</h1>
           <h3>Please check back later</h3>
