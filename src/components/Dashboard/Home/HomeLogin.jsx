fb918aa75227369c308509e9f3f46b89b44cc8a0
Andrew Martins <andrew_omartins@yahoo.com>
2021-04-06 12:58:11 -0400

    fixed the spacing between event cards

--- src/components/Dashboard/Home/HomeLogin.jsx
+++ src/components/Dashboard/Home/HomeLogin.jsx
@@ -177,10 +177,6 @@ export default function HomeLogin() {
                 id="password"
                 autoComplete="current-password"
               />
-              <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="Remember me"
-              />
               <Button
                 type="submit"
                 fullWidth
@@ -192,14 +188,8 @@ export default function HomeLogin() {
               </Button>
               <Grid container>
                 <Grid item xs>
-                  <Link href="#" variant="body2">
-                    Forgot password?
-                  </Link>
                 </Grid>
                 <Grid item>
-                  <Link href="#" variant="body2">
-                    {"Don't have an account? Sign Up"}
-                  </Link>
                 </Grid>
               </Grid>
               <Box mt={5}>
