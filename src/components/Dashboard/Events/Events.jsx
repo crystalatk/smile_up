7a974f195141674424704671834878640e5d7d61
Crystal Atkinson <crystaltayloratkinson@gmail.com>
2021-04-07 12:39:55 -0400

    Styling for the event list complete

--- src/components/Dashboard/Events/Events.jsx
+++ src/components/Dashboard/Events/Events.jsx
@@ -1,4 +1,9 @@
+import { useState } from "react";
 import { Route } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
 import EventList from "./EventList";
 import EventDetails from "./EventDetails";
 import EditEvent from "./EditEvent";
@@ -7,15 +12,46 @@ import MinorSignUp from "./MinorSignUp";
 import CheckIn from "./CheckIn";
 import CheckOut from "./CheckOut";
 
+const UPCOMING = "Upcoming Events";
+const PAST = "Past Events";
+
+const useStyles = makeStyles({
+  root: {
+    flexGrow: 1,
+  },
+});
+
 const Events = ({
   userInfo,
   setEventDetailsForEditPurposes,
   eventDetailsForEditPurposes,
 }) => {
+  const classes = useStyles();
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
   return (
     <>
       <Route exact path="/events">
-        <EventList />
+        <Paper className={classes.root}>
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            indicatorColor="primary"
+            textColor="primary"
+            centered
+          >
+            <Tab label={UPCOMING} />
+            <Tab label={PAST} />
+          </Tabs>
+        </Paper>
+        {value ? (
+          <EventList userInfo={userInfo} />
+        ) : (
+          <EventList userInfo={userInfo} />
+        )}
       </Route>
       <Route exact path="/events/editevent">
         <EditEvent eventDetailsForEditPurposes={eventDetailsForEditPurposes} />
--- src/components/Nav/NotificationIcon.jsx
+++ src/components/Nav/NotificationIcon.jsx
@@ -1,29 +1,17 @@
-import { useState, useEffect } from "react";
-import { Link, useLocation } from "react-router-dom";
-import { makeStyles } from "@material-ui/core/styles";
+import { useEffect } from "react";
 import NotificationsActiveIcon from "@material-ui/icons/NotificationsActive";
 import Badge from "@material-ui/core/Badge";
-
-const useStyles = makeStyles({
-  badge: {},
-});
 
 const NotificationIcon = ({
   numberOfApprovalsWaiting,
   setNumberOfApprovalsWaiting,
 }) => {
-  const classes = useStyles();
-
   useEffect(() => {
     setNumberOfApprovalsWaiting(0);
   }, []);
 
   return (
-    <Badge
-      badgeContent={numberOfApprovalsWaiting}
-      color="primary"
-      className={classes.badge}
-    >
+    <Badge badgeContent={numberOfApprovalsWaiting} color="primary">
       <NotificationsActiveIcon />
     </Badge>
   );
