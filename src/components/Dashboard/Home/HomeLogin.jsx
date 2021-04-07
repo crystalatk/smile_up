7240411795a60d40764be4ff69adc1065b0a2491
Andrew Martins <andrew_omartins@yahoo.com>
2021-04-07 10:32:25 -0400

    Creating a landing page

--- src/components/Dashboard/Home/HomeLogin.jsx
+++ src/components/Dashboard/Home/HomeLogin.jsx
@@ -3,9 +3,6 @@ import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
-import Link from "@material-ui/core/Link";
 import Paper from "@material-ui/core/Paper";
 import Box from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
@@ -14,7 +11,6 @@ import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import { Zoom } from "react-slideshow-image";
 import "react-slideshow-image/dist/styles.css";
-import smileupkids from "../../../images/smileupkids.jfif";
 import smile2 from "../../../images/smile2.jfif";
 import s4 from "../../../images/s4.jpg";
 import s5 from "../../../images/s5.jpg";
@@ -64,17 +60,17 @@ const Slideshow = () => {
 };
 const useStyles = makeStyles((theme) => ({
   root: {
-    height: "75vh",
+    minHeight: "100vh",
+   
   },
   image: {
-    backgroundImage: `url(${smileupkids})`,
-    backgroundRepeat: "no-repeat",
     backgroundColor:
       theme.palette.type === "light"
         ? theme.palette.grey[50]
         : theme.palette.grey[900],
     backgroundSize: "contain",
-    maxHeight: "100vw",
+    maxHeight: "100vh", 
+    width: "100vw",
     overflow: "hidden",
     backgroundPosition: "center",
   },
@@ -89,15 +85,11 @@ const useStyles = makeStyles((theme) => ({
     maxHeight: "100%",
     borderRadius: "16",
     borderWidth: "5",
-    borderColor: "green",
     overflow: "hidden",
     backgroundPosition: "center",
-    borderLeftWidth: "9px",
-    borderLeftColor: "rgb(0,214,203)",
-    borderLeftStyle: "solid",
   },
   paper: {
-    margin: theme.spacing(8, 4),
+    margin: theme.spacing(8, 9, 5, 9),
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
@@ -129,8 +121,6 @@ export default function HomeLogin() {
         <Grid
           item
           xs={12}
-          sm={8}
-          md={5}
           component={Paper}
           elevation={6}
           square
